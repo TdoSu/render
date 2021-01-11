ef8e45907d9deb66606585e4a2fd74ba59cae221
weifubao <weifubao87@163.com>
2021-01-11 10:05:27 +0800

    异步加载路由

--- src/main.js
+++ src/main.js
@@ -1,12 +1,51 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import generatorRouter from './router'
 import store from './store'
+
+import Home from './views/Home.vue'
+import About from './views/About.vue'
 
 Vue.config.productionTip = false
 
-new Vue({
-    router,
-    store,
-    render: h => h(App),
-}).$mount('#app')
+const path2Component = path => ({
+    './views/Home.vue': Home,
+    './views/About.vue': About,
+})[path]
+
+const getRoutes = function () {
+    return new Promise((resolve, reject) => {
+        const routes = [
+            {
+                pagePath: '/',
+                pageName: 'Home',
+                componentPath: './views/Home.vue',
+            },
+            {
+                pagePath: '/about',
+                pageName: 'About',
+                componentPath: './views/About.vue',
+            },
+        ]
+        setTimeout(_ => {
+            resolve(routes)
+        }, 500)
+    })
+}
+
+getRoutes()
+    .then(res => {
+        const routes = res.map(r => {
+            return {
+                path: r.pagePath,
+                name: r.pageName,
+                component: path2Component(r.componentPath),
+            }
+        })
+        const router = generatorRouter(routes)
+        new Vue({
+            router,
+            store,
+            render: h => h(App),
+        }).$mount('#app')
+    })
